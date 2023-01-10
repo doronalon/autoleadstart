c6fb8804c3e76f7c5fabf4c82710c04e134560a7
doronalon <doron.alon.email@gmail.com>
2023-01-10 10:36:29 +0200

    Add files via upload

--- src/sorted.paragraph.ts
+++ src/sorted.paragraph.ts
@@ -1,33 +1,44 @@
 // const alphabet: string[] = "abcdefghijklmnopqrstuvwxyz".split("");
 const hebrew: string [] = "ABGDHVZJTYKLMNSIPXQRWUCEFO".toLocaleLowerCase().split("")
 
-const compare = (a: string, b: string, order : string[] = hebrew) : number => {
+const compare = (a: string, b: string, order: string[] = hebrew): number => {
+
     let index: number = 0
-    while (a.slice(index, index + 1) || b.slice(index, index + 1)) {
-        if (order.indexOf(a.slice(index, index + 1)) === order.indexOf(b.slice(index, index + 1))) {
+    let aLower: string = a.toLocaleLowerCase()
+    let bLower: string = b.toLocaleLowerCase()
+    while (aLower.slice(index, index + 1) || bLower.slice(index, index + 1)) {
+        if (order.indexOf(aLower.slice(index, index + 1)) === order.indexOf(bLower.slice(index, index + 1))) {
             ++index
         } else {
             // > 0 => sort a after b
             // < 0 => sort a before b
-            return order.indexOf(a.slice(index, index + 1)) - order.indexOf(b.slice(index, index + 1))
+            return order.indexOf(aLower.slice(index, index + 1)) - order.indexOf(bLower.slice(index, index + 1))
         }
     }
     return 0 //keep original order of a and b
 }
 
 export const sortedParagraph = (paragraph: string): string => {
+    try {
+        if (typeof paragraph === "string") {
+            return paragraph.trim() //removes whitespace from both ends of a string
+                //remove the punctuation from the input => replace any character that is not a word character or a space character with nothing.
+                // word character is a character a-z, A-Z, 0-9, including _ (underscore)
+                .replace(/[^\w\s]/g, "")
+                .split(/\s+/)
+                .sort((a, b) => compare(a, b))
+                .join(" ")
+        } else {
+            return `Invalid paragraph ${paragraph}, paragraph type is: ${typeof paragraph}`
+        }
+    } catch (e) {
+        return e.message
+    }
 
-    return paragraph.trim() //removes whitespace from both ends of a string
-        .toLocaleLowerCase()
-        //remove the punctuation from the input => replace any character that is not a word character or a space character with nothing.
-        // word character is a character a-z, A-Z, 0-9, including _ (underscore)
-        .replace(/[^\w\s]/g, "")
-        .split(/\s+/)
-        .sort((a, b) => compare(a, b))
-        .join(" ")
+
 }
 
 
-console.info(sortedParagraph('Lorem ipsum, dolor!!! sit amet, consectetur10!!?.. adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna gg aliqua'))
+// console.info(sortedParagraph('Lorem ipsum, dolor!!! sit amet, consectetur10!!?.. adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna gg aliqua'))
 
 
--- test/sorted.paragrapph.spec.ts
+++ test/sorted.paragrapph.spec.ts
@@ -3,9 +3,20 @@ import {sortedParagraph} from "../src/sorted.paragraph";
 describe('sortedParagraph group', () => {
 
     it('remove punctuation from the input and sorted by hebrew.', () => {
-       let str: string = 'Lorem ipsum, dolor!!! sit amet, consectetur10!!?.. adipiscing elit'
-        expect(sortedParagraph(str)).toEqual('adipiscing amet dolor lorem sit ipsum consectetur10 elit')
+       let str: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        expect(sortedParagraph(str)).toEqual('adipiscing aliqua amet do dolor dolore tempor ' +
+            'labore Lorem magna sit sed incididunt ipsum ut consectetur et elit eiusmod')
     })
 
+    it('empty string', () => {
+        let str: string = ''
+        expect(sortedParagraph(str)).toEqual('')
+    })
+
+    it('undefined', () => {
+        let str: string = undefined
+        expect(sortedParagraph(str)).toEqual('Invalid paragraph undefined, paragraph type is: undefined')
+    })
 
 })
